10095bad59953cb704fcc737cc32432a37b971ed
atsukan <c0a2205071@edu.teu.ac.jp>
2023-05-30 17:07:23 +0900

    完成版

--- kadaipuroguram.py
+++ kadaipuroguram.py
@@ -13,7 +13,7 @@ blue = (0,0,255)
 light_blue = (147,251,253)
 width = 1600 # ディスプレイの横の長さ
 height = 900 # ディスプレイの縦の長さ
-goalheight = 50 # ゴールの範囲
+goalheight = 150 # ゴールの範囲
 
 def check_bound(area: pg.Rect, obj: pg.Rect) -> tuple[bool, bool]:
     yoko, tate = True, True
@@ -55,8 +55,8 @@ class playerlect: # パドルに関するクラス
     
 
     def __init__(self, xy: tuple[int,int], zw: tuple[int,int]): 
-        self._img1 = pg.transform.rotozoom(pg.image.load(f"ProjExD2023/ex05/redpad.png"),0, 2.0)
-        self._img2 = pg.transform.rotozoom(pg.image.load(f"ProjExD2023/ex05/bluepad.png"),0, 2.0)
+        self._img1 = pg.transform.rotozoom(pg.image.load(f"ex05/redpad.png"),0, 2.0)
+        self._img2 = pg.transform.rotozoom(pg.image.load(f"ex05/bluepad.png"),0, 2.0)
         self._rct1 = self._img1.get_rect()
         self._rct2 = self._img2.get_rect()
         self._rct1.center = xy
@@ -86,7 +86,7 @@ class playerlect: # パドルに関するクラス
 class ball: # ディスクに関するクラス
     _dires = [-1, +1]
     def __init__(self):
-        self._img = pg.image.load(f"ProjExD2023/ex05/disc.png")
+        self._img = pg.image.load(f"ex05/disc.png")
         self._rct = self._img.get_rect()
         self._rct.center = width/2,height/2
         self._vx, self._vy = random.choice(ball._dires), random.choice(ball._dires)
@@ -100,11 +100,11 @@ class ball: # ディスクに関するクラス
 
         if pl._rct1.colliderect(self._rct):
             self._vx *= -1
-            self._vy *= -1
+            self._vy *= random.choice(ball._dires)
         
         if pl._rct2.colliderect(self._rct):
             self._vx *= -1
-            self._vy *= -1
+            self._vy *= random.choice(ball._dires)
 
 
         self._rct.move_ip(self._vx, self._vy)
@@ -157,15 +157,15 @@ def main():
         pg.draw.line(screen, red, (screen.get_width(),0), (screen.get_width(),screen.get_height()/2-goalheight) ,5)
         pg.draw.line(screen, red, (screen.get_width(),screen.get_height()/2 + goalheight), (screen.get_width(),screen.get_height()) ,5)
 
-        #ゴール判定 左ゴールに入った場合、g_leftがTrueに,右ゴールに入った場合、g_rightがTrueにそれぞれなる
+        # ゴール判定 左ゴールに入った場合、g_leftがTrueに,右ゴールに入った場合、g_rightがTrueにそれぞれなる
         g_left, g_right = disc.check_goal_in(screen.get_rect(), goalheight)
         if g_left == True:
-            score1 += 1
+            score2 += 1  # 得点機能
             disc = ball()
         if g_right == True:
-            score2 += 1
+            score1 += 1
             disc = ball()
-        if score1 == 10 or score2 == 10:
+        if score1 == 5 or score2 == 5:
             txt2 = fonto3.render("Game Set!", True, (255, 0, 0)) 
             screen.blit(txt2, [700, 200]) 
             pg.display.update()
@@ -174,7 +174,7 @@ def main():
 
         key_lst = pg.key.get_pressed()
         
-        pl1.update(key_lst,screen)
+        pl1.update(key_lst,screen)  # 得点の表示
         txt = fonto.render(str(int(score1)), True, (255, 255, 255))
         screen.blit(txt, [400, 200])
         txt2 = fonto2.render(str(int(score2)), True, (255, 255, 255))  
